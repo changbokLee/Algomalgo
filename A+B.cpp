8798368ee695296eacd7a91adb1a48384294fcbe
changbokLee <changbok2453@gmail.com>
2021-03-13 20:18:20 +0900

    basic

---
+++ A+B.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){ 
+    int first , second;
+    scanf("%d %d" , &first , &second);
+    printf("%d %d\n" , first ,second);
+    return 0;
+}
---
+++ squre.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+  //Please Enter Your Code Here
+  int  a , b;
+  
+  scanf("%d %d" , &a , &b);
+  printf("%d * %d : %d" , a , b , a*b);
+  return 0;
+}
