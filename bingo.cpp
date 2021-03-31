90a5ee5a2a1f65de476c148020629be3d0bf1971
changbokLee <changbok2453@gmail.com>
2021-04-01 00:38:22 +0900

    dd

--- bingo.cpp
+++ bingo.cpp
@@ -5,7 +5,72 @@ int main() {
   //Please Enter Your Code Here
   int arr[5][5];
 
-  for(int i )
+  for(int i=0; i<5; i++){
+      for(int j =0; j<5; j++){
+       scanf("%d" , &arr[i][j]);
+      }
+
+int order= 0;
+for(int i=0; i<5; i++){
+      for(int j =0; j<5; j++){
+       int a;
+
+       scanf("%d", &a);
+        order++;
+
+
+for(int k=0; k<5; k++){
+    for(int p=0; p<5; p++){
+        if(arr[k][p]==a){
+            arr[k][p]= -1;
+        }
+    }
+
+}
+
+int cnt =0;
+
+for(int k=0;k<5;k++){
+    int Xcnt =0;
+    for(int p=0;p<5;p++){
+        if(arr[k][p] == -1){
+            Xcnt++;
+        }
+    }
+
+    if(Xcnt ==5) cnt++;
+
+}
+for(int k=0;k<5;k++){
+    int Xcnt =0;
+    for(int p=0;p<5;p++){
+        if(arr[p][k] == -1){
+            Xcnt++;
+        }
+    }
+    if(Xcnt ==5) cnt++;
+}     
+if(arr[0][0] == -1 && arr[1][1] == -1 && arr[2][2] ==-1 && arr[3][3]==-1&& arr[4][4]==-1)
+      cnt++;
+if(arr[0][4] == -1 && arr[1][3] == -1 && arr[2][2]==-1&& arr[3][1] ==-1&& arr[4][0]==-1)
+ cnt++;
+
+ if(cnt >=3){
+     printf("%d\n",order);
+     return 0;
+ }
+      }
+
+}
+
+
+
+          // 1) a의 위치
+          // 2) a를 지워야함
+          // 3) 몇개의줄이 지워졌는지 파악
+          // 4) 3개이상의 줄이지워졌다면끄,ㅌ
+      
+  
 
   return 0;
 }
--- colorpaper.cpp
+++ colorpaper.cpp
@@ -4,6 +4,28 @@ int main() {
 
   //Please Enter Your Code Here
 
+  int n;
+  int plane[101][101] ={0,};
+  
+  scanf("%d",&n);
+  
+  for(int i = 0; i<n; i++){
+    int a, b, c, d;
+    
+    scanf("%d %d %d %d",&a ,&b ,&c ,&d );
+    //a : 가로 b: 세로 c: 가로길이 
+   //2 3, 2 5 j 
+  
+  for(int  j =b; j<b+d; j++){
+    for(int k =a; k<a+c; k++){
+      plane[j][k] = i+1;
+    }
+  }
+  
+  }
+    
+  
+
+
   return 0;
 }
-
