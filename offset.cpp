ebc8b7d79b29ff0c9df9d1d1e82d5e3e1c8b3a08
changbokLee <changbok2453@gmail.com>
2021-04-01 18:00:18 +0900

    호우

--- offset.cpp
+++ offset.cpp
@@ -3,6 +3,8 @@
 int main() {
 
   //Please Enter Your Code Here
+
+  //표 그리기//
   int arr[10][10];
   
   for(int i =1; i<=5; i++){
@@ -10,11 +12,17 @@ int main() {
       scanf("%d", &arr[i][j]);
     }
   }
-  for(int i = 0; i<=6; i++) arr[0][i] = 99;
+// 기본틀 //
+
+
+  for(int i = 0; i<=6; i++) arr[0][i] = 99; // 왜 99지??? 
   for(int i =0; i<=6; i++)arr[6][i] = 99;
   for(int i =0; i<=6; i++)arr[i][0] = 99;
   for(int i =0; i<=6; i++)arr[i][6] = 99;
   
+  //들어가는 값의 수 를 나타냄
+
+  // 여기에 값이들어가면 어떻게 작동될지 값을 출력함//
   for(int i=1; i<=5; i++){
     for(int j = 1; j<=5; j++){
       if(arr[i-1][j] > arr[i][j]&&
@@ -23,6 +31,8 @@ int main() {
         arr[i+1][j] > arr[i][j])printf("* ");
         else printf("%d " ,arr[i][j]);
     }
+
+    
     
     printf("\n");
   }
