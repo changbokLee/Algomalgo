50c13b38195486ebe48f6c2fc50fb27a135c0413
changbokLee <changbok2453@gmail.com>
2021-04-08 17:44:40 +0900

    백업

---
+++ 합병정렬.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+
+  //Please Enter Your Code Here
+
+  int n;
+  int number[100];
+
+  scanf("%d" , &n);
+
+  for(int i = 0; i<n; i++){
+      scanf
+  }
+
+  return 0;
+}
