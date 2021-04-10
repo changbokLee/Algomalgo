a5c037595cd35a44ffb5fb3639089ebd6e2adc12
changbokLee <changbok2453@gmail.com>
2021-04-10 10:48:50 +0900

    halo

---
+++ 단순밀기.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+
+  //Please Enter Your Code Here
+   int n;
+   int arr[1000];
+
+   scanf("%d" , &n);
+
+   for(int i = 0; i<n; i++){
+       scanf("%d" , &arr[i]);
+   }
+
+    for(int i = 0; i<n; i++){
+       scanf("%d" , &arr[i]);
+   }
+
+  return 0;
+}
