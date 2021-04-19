62e06528315d813b7951ea3d3a875172c880f734
changbokLee <changbok2453@gmail.com>
2021-04-19 10:18:58 +0900

    개어려웡

---
+++ NN단표.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+
+  //Please Enter Your Code Here
+  
+
+  return 0;
+}
+
+
+// 1 2 3 4 
+// 2 4 6 8
+// 3 6 9 12
+// 4 8 12 16 k=8
+
+
+// 1 2 2 3 4  4 4 6 6  8 8 9 12 12 16
