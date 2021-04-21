3599730f31de492b817d6fc0c4e6b02a68fbd897
changbokLee <changbok2453@gmail.com>
2021-04-21 15:09:02 +0900

    허ㅏ하

---
+++ 연속부분최대합.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+using namespace std;
+
+int n;
+int data[MAX];
+
+
+
+
+
+int main() {
+
+  //Please Enter Your Code Here
+    scanf("%d" ,&n);
+
+    for(int i =0; i<n;i++){
+        scanf("%D")
+    }
+   
+    
+  return 0;
+}
