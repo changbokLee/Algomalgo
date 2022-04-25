137556da35c5662d05d4560a1d07f8d701926ad2
changbokLee <changbok2453@gmail.com>
2022-04-25 19:57:31 +0900

    수정

--- 삼각형만들기.cpp
+++ 삼각형만들기.cpp
@@ -1,16 +1,17 @@
 #include <stdio.h>
-//
+
 int main() {
 
   //Please Enter Your Code Here
   int n;
   scanf("%d",&n);
-  for(int i=0;i<=n;i++){
-    for(int j=0;j<=n;i++){
-      printf("*");
+  for(int i=0;i<n;i++){
+    for(int j=0;j<=i;j++){
+       printf("*");
     }
       printf("\n");
-  }
+    }
+ 
 
   return 0;
 }
